7b11941aac601f8ed7ef5073b8fe3809800fcbd1
root <root@ssdvps-98146.vps.net>
2019-11-28 13:11:12 +0000

    index.php: ff wat comments :pistol:

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 session_start();
 
-if(!isset($_SESSION['user_ID']))
+if(!isset($_SESSION['user_ID'])) // user is not logged in
 {
     header("Location: html/login.html");
     exit();
-}else
+}else // user is logged in
 {
     header("Location: html/dashboard.html");
 }
+
+
